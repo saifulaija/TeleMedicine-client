7de41fac3c6b80d91537b718e8266ecdd5a77dc7
saifulaija <saifulaija2023@gmail.com>
2024-05-10 18:26:42 +0600

    logout functionality done

--- src/components/Shared/UI/AuthButton/AuthButton.tsx
+++ src/components/Shared/UI/AuthButton/AuthButton.tsx
@@ -1,27 +1,28 @@
+import logoutUser from "@/services/actions/logoutUser";
 import { getUserInfo, removeUser } from "@/services/auth.services";
 import { Button } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-
 const AuthButton = () => {
-    const userInfo=getUserInfo();
-    const router =useRouter();
-    const handleLogout=()=>{
-      removeUser();
-      router.refresh()
-    }
+  const userInfo = getUserInfo();
+  const router = useRouter();
+  const handleLogout = () => {
+    logoutUser(router);
+  };
   return (
     <>
-     {
-        userInfo?.userId ?  <Button color="warning" onClick={()=>handleLogout()} >
-        Logout
-      </Button>: <Button  component={Link} href="/login">
+      {userInfo?.userId ? (
+        <Button color="warning" onClick={() => handleLogout()}>
+          Logout
+        </Button>
+      ) : (
+        <Button component={Link} href="/login">
           Login
         </Button>
-       }
+      )}
     </>
-  )
-}
+  );
+};
 
-export default AuthButton
+export default AuthButton;
